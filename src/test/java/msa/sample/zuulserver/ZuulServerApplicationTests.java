d176e598f5075b2e3a31e2fc0761fd447695c0b1
cdckyt <cdckyt@naver.com>
2024-01-09 15:33:01 +0900

    전체 소스 커밋

---
+++ src/test/java/msa/sample/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.sample.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
