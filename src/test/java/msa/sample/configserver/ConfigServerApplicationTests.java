65f46b45afedc73d9ff64fd324d19c1e733459e3
cdckyt <cdckyt@naver.com>
2024-01-09 15:18:31 +0900

    전체 소스 커밋

---
+++ src/test/java/msa/sample/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.sample.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
